47dd7ecabb0c2dd6cc8007fc3284bc68e3ae8714
alnusglutinosa <olga.app2@gmail.com>
2019-01-08 02:44:27 +0200

    hw4

--- dz1/js/foundations.js
+++ dz1/js/foundations.js
@@ -3,13 +3,17 @@ let string = 'some test string';
 
 // 1. Получить первую и последнюю буквы строки
 console.log(string[0]);
-console.log(string.slice(-1));
+console.log(string.slice(-1)); //string[string.length - 1];
 
 // 2. Сделать первую и последнюю буквы в верхнем регистре
 console.log(string[0].toUpperCase() + string.slice(1,-1) + string.slice(-1).toUpperCase());
+// string[0].toUpperCase();
+// string[string.length - 1].toUpperCase();
 
 // 3. Найти положение слова ‘string’ в строке
-console.log(string.indexOf('string') + 1);
+//console.log(string.indexOf('string') + 1);
+
+let secondSpace = string.indexOf(' ', string.indexOf(' ') + 1);
 
 // 4. Найти положение второго пробела (“вручную” ничего не считать)
 console.log(string.lastIndexOf(' ') + 1);
@@ -36,6 +40,7 @@ console.log(string);
 // 1. Получить число pi из Math и округлить его до 2-х знаков после точки
 let pi = Math.PI;
 console.log( Math.round(pi * 100) / 100 );
+// parseFloat((Math.PI).toFixed(2));
 
 // 2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
 console.log(Math.max(15, 11, 16, 12, 51, 12, 13, 51));
@@ -45,8 +50,9 @@ console.log(Math.min(15, 11, 16, 12, 51, 12, 13, 51));
 // a. Получить случайное число и округлить его до двух цифр после запятой
 // b. Получить случайное целое число от 0 до X. X - любое произвольное число. 
 let randomNumber = Math.random() * 20;
-randomNumber = randomNumber.toFixed(2);
-console.log(randomNumber);
+// randomNumber = randomNumber.toFixed(2);
+// console.log(randomNumber);
+parseFloat(randomNumber.toFixed(2));
 
 let x = 15;
 randomNumber = Math.round(Math.random() * x);
@@ -60,3 +66,5 @@ console.log( (a * 10 + b * 10) / 10 );
 
 // 5. Получить число из строки ‘100$’
 console.log(parseInt('100$'));
+
+// parseFloat((0.6 + 0.7).toFixed(1));
---
+++ dz4/js/function.js
@@ -0,0 +1,191 @@
+// Функции. Задачи.
+// 1. Создать функцию multiply, которая будет принимать любое количество чисел и возвращать их произведение: multiply(1,2,3) = 6 (1*2*3)
+//  Если нет ни одного аргумента, вернуть ноль: multiply() // 0
+
+function multiply() {
+    if (!arguments.length) {
+        return 0;
+    }
+
+    let result = 1;
+
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (typeof arguments[i] !== "number" || isNaN(arguments[i])) {
+            continue;
+        }
+
+        result *= arguments[i];
+    } 
+
+    return result;
+}
+
+console.log('1.', multiply(1, 2, 3));
+
+
+// 2. Создать функцию, которая принимает строку и возвращает строку-перевертыш: reverseString(‘test’) // “tset”.
+
+function reverseString(stringVariable) {
+    if (typeof stringVariable !== "string") {
+        return console.log('function reverseString: stringVariable - not string');
+    }
+
+    let reversRes = stringVariable.split('');
+    reversRes.reverse();
+    reversRes = reversRes.join('');
+
+    return reversRes;
+}
+
+console.log('2.', reverseString('test'));
+
+// 3. Создать функцию, которая в качестве аргумента принимает строку из букв и возвращает строку, 
+// где каждый символ разделен пробелом и заменен на юникод-значение символа: 
+// getCodeStringFromText(‘hello’) // “104 101 108 108 111” 
+// подсказка: для получения кода используйте специальный метод 
+
+function getCodeStringFromText(stringVariable) {
+    if (typeof stringVariable !== "string") {
+        return console.log('function getCodeStringFromText: stringVariable - not string');
+    }
+
+    let unicodeArray = stringVariable.split('');
+
+    for(let i = 0; i < unicodeArray.length; i++) {
+        unicodeArray[i] = unicodeArray[i].charCodeAt(0);
+    }
+  
+    return unicodeArray.join(' ');
+}
+
+console.log('3.', getCodeStringFromText('hello'));
+
+
+// 4. Создать функцию угадай число. Она принимает число от 1-10 (обязательно проверить что число не больше 10 и не меньше 0). 
+// Генерирует рандомное число от 1-10 и сравнивает с переданным числом если они совпали то возвращает “Вы выиграли” 
+// если нет то “Вы не угадали ваше число 8 а выпало число 5”. Числа в строке указаны как пример вы подставляете реальные числа.
+
+function guessNumber(userNumer) {
+
+    if (typeof userNumer !== "number" || isNaN(userNumer)) {
+        return "Вы ввели не число";
+    }
+
+    if (userNumer > 10 || userNumer <= 0) {
+        return "Введите число от 1-10";
+    }
+
+    randomNumber = Math.round(Math.random() * 10);
+
+    if (randomNumber !== userNumer) {
+        return `Вы не угадали ваше число ${userNumer} а выпало число ${randomNumber}`;
+    } 
+            
+    return "Вы выиграли";
+}
+
+console.log('4.', guessNumber(7));
+
+
+// 5. Создать функцию, которая принимает число N и возвращает массив, заполненный числами от 1 до N: getArray(10); // [1,2,3,4,5,6,7,8,9,10]
+
+function getArray(lengthArray) {
+
+    if (typeof lengthArray !== "number" || isNaN(lengthArray)) {
+        return console.log("function getArray : lengthArray - not number");
+    }
+
+    let arrayVariable = [];
+
+    for(let i = 1; i <= lengthArray; i++) {
+        arrayVariable.push(i);
+    }
+    return arrayVariable;
+}
+
+console.log('5.', getArray(10));
+
+
+// 6. Создать функцию, которая принимает массив, а возвращает новый массив с дублированными элементами входного массива:
+// doubleArray([1,2,3]) // [1,2,3,1,2,3]
+
+function doubleArray(arrayNumbers) {
+
+    if (!Array.isArray(arrayNumbers)) {
+        return console.log("function doubleArray : arrayNumbers - not Array");
+    }
+
+    let arrayCopy = arrayNumbers.slice();
+    return arrayCopy.concat(arrayNumbers);
+}
+
+console.log('6.', doubleArray([1,2,3])); 
+
+
+// 7. Создать функцию, которая принимает произвольное (любое) число массивов и удаляет из каждого массива первый элемент, 
+// а возвращает массив из оставшихся значений: 
+// changeCollection([1,2,3], [‘a’, ’b’, ‘c’]) → [ [2,3], [‘b’, ‘c’] ], changeCollection([1,2,3]) → [ [2,3] ] и т.д.
+
+function changeCollection() {
+    if (!arguments.length) {
+        return console.log("function changeCollection : no argument");
+    }
+
+    arrayResult = [];
+
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (!Array.isArray(arguments[i])) {
+            continue;
+        }
+
+        arguments[i].shift();
+        arrayResult.push(arguments[i]);
+    } 
+
+    return arrayResult;
+}
+
+console.log('7.', changeCollection([1,2,3], ['a', 'b', 'c']));
+
+
+// 8. Создать функцию которая принимает массив пользователей, поле на которое хочу проверить и значение на которое хочу проверять. 
+//Проверять что все аргументы переданы. Возвращать новый массив с пользователями соответсвующие у казанным параметрам.
+
+// funcGetUsers(users, “gender”, “male”); // [ {name: “Denis”, age: “29”, gender: “male”} , {name: “Ivan”, age: “20”, gender: “male”} ]
+
+function funcGetUsers(arrayUsers, checkField, checkValue) {
+    if (!Array.isArray(arrayUsers)) {
+        return console.log("function funcGetUsers : arrayUsers not array");
+    }
+
+    if (typeof checkField !== "string") {
+        return console.log('function funcGetUsers: checkField - not string');
+    }
+
+    if (!checkValue) {
+        return console.log('function funcGetUsers: checkValue - no argument');
+    }
+
+    let arrayCheck = [];
+
+    for (let i = 0; i < arrayUsers.length; i++) {
+        if (arrayUsers[i][checkField] === checkValue) {
+            arrayCheck.push(arrayUsers[i]);
+        }
+    }
+  
+    return arrayCheck 
+}
+
+let users = [ 
+    {name: 'Denis', age: '27', gender: 'male'}, 
+    {name: 'Ira', age: '38', gender: 'female'},
+    {name: 'Ivan', age: '50', gender: 'male'},
+    {name: 'Anna', age: '26', gender: 'female'},
+    {name: 'Olha', age: '27', gender: 'female'} 
+];
+
+
+console.log('8.', funcGetUsers(users, "gender", "male"));
